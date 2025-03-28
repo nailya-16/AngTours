2a094582c9352c92d5d1722f8c8cae35eb62c0a2
Nailya <nailya-16@users.noreply.github.com>
2025-03-28 18:14:50 +0300

    Implemented processing of requests to the server with the Registration and Authorization components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,6 @@ import { LayoutComponent } from './layout/layout.component';
 export const routes: Routes = [
     { path: 'auth', component: AuthComponent },
     { path: '', redirectTo: '/auth', pathMatch: 'full' },
-    
+    { path: 'tickets', component: LayoutComponent},
     { path: '**', redirectTo: '/auth', pathMatch: 'full' },
 ];
--- src/app/pages/auth/authorization/authorization.component.ts
+++ src/app/pages/auth/authorization/authorization.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { IUser } from '../../../models/user';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-authorization',
-  imports: [],
+  imports: [NgClass, FormsModule, ButtonModule, InputTextModule],
   templateUrl: './authorization.component.html',
-  styleUrl: './authorization.component.scss'
+  styleUrls: ['./authorization.component.scss'],
 })
-export class AuthorizationComponent {
+export class AuthorizationComponent implements OnInit, OnDestroy {
+  login: string = '';
+  password: string = '';
 
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
+
+  onAuth(): void {
+    const user: IUser = {
+      login: this.login,
+      password: this.password,
+    };
+
+    this.userService.authUser (user).subscribe({
+      next: (response) => {
+        this.router.navigate(['tickets']);
+        this.initToast('success', 'Авторизация прошла успешно');
+      },
+      error: (error) => {
+        console.error('Ошибка при авторизации:', error);
+        this.initToast('error', error.error.message || 'Ошибка авторизации. Попробуйте еще раз.');
+      }
+    });
+  }
+
+  initToast(type: 'error' | 'success', text: string): void {
+    this.messageService.add({ severity: type, detail: text, life: 3000 });
+  }
 }
