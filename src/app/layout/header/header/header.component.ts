94c3cf56cac237dd2f5ec7ac5fb0d28c3634fd8c
Nailya <nailya-16@users.noreply.github.com>
2025-03-31 01:23:38 +0300

    Using DatePipe in Header

--- src/app/layout/header/header/header.component.ts
+++ src/app/layout/header/header/header.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  //encapsulation: ViewEncapsulation.None,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  dateTime: Date;
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+      this.dateTime = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {}
 }
