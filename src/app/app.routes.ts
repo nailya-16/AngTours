645b549472a618518dfde0fb5d240313f9512e28
Nailya <nailya-16@users.noreply.github.com>
2025-04-01 14:59:47 +0300

    Creating a Tour-Item Component. Using the Activated Route Class

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ToursComponent } from './pages/tours/tours.component';
+import { TourItemComponent } from './pages/tour-item/tour-item.component';
 
 export const routes: Routes = [
     { path: 'auth', component: AuthComponent },
@@ -9,7 +10,9 @@ export const routes: Routes = [
     { path: 'tours',
         component: LayoutComponent,
         children: [
-            {path: '', component: ToursComponent}
+            { path: '', component: ToursComponent,},
+            { path: 'tour', redirectTo: '', pathMatch: 'full'},
+            { path: 'tour/:id', component: TourItemComponent}
         ]
     },
 
--- src/app/pages/tours/tours.component.ts
+++ src/app/pages/tours/tours.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ToursService } from '../../services/tours.service';
 import { CardModule} from 'primeng/card'
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tours',
@@ -11,7 +12,9 @@ import { CardModule} from 'primeng/card'
 export class ToursComponent implements OnInit { 
   tours: any = []; //TODO add types
 
-  constructor(private toursService: ToursService){}
+  constructor(private toursService: ToursService,
+    private route: ActivatedRoute,
+    private router: Router){}
 
   ngOnInit(): void {
     this.toursService.getTours().subscribe((data) => {
@@ -19,5 +22,9 @@ export class ToursComponent implements OnInit {
         this.tours = data.tours;
       }
     });
+  }
+
+  goToTour(item: any): void {   //TODO add interface
+    this.router.navigate(['tour', item.id], {relativeTo: this.route});
   }
 }
