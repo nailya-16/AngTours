ccd12ef4325b4418fd65299ee28b359add62a23a
Nailya <nailya-16@users.noreply.github.com>
2025-04-24 00:26:12 +0300

    Http Interceptors. Creating an ErrorInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,10 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura'
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ConfigService } from './services/config.service';
+import { errorInterceptor } from './shared/interceptors/error.interceptor';
+import { MessageService } from 'primeng/api';
 
 function initializeApp(config: ConfigService) {
   return config.loadPromise();
@@ -23,11 +25,12 @@ export const appConfig: ApplicationConfig = {
         dayNames: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
         monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
         dayNamesMin: ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'],
-        today: 'Сегодня',
-        clear: 'Очистка'
+        today: 'Текущая дата',
+        clear: 'Очистить'
       }
     }),
-    provideHttpClient(),
-    provideAppInitializer(() => initializeApp(inject(ConfigService)))
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideAppInitializer(() => initializeApp(inject(ConfigService))),
+    MessageService
   ]
 };
