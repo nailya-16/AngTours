8b06d5ec905c8d6c448db3b0d42b8f418853ec68
Nailya <nailya-16@users.noreply.github.com>
2025-04-27 21:16:18 +0300

    Order - Introduction to ReactiveForms

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -2,16 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Tour } from '../../models/tours';
 import { ToursService } from '../../services/tours.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber'
+import { InputTextModule } from 'primeng/inputtext';
+import { DatePickerModule } from 'primeng/datepicker';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-order',
-  imports: [RouterLink],
+  imports: [RouterLink,
+    ReactiveFormsModule,
+    InputNumberModule,
+    InputTextModule,
+    DatePickerModule,
+    ButtonModule
+  ],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
 export class OrderComponent implements OnInit {
   tourId: string = null;
   tour: Tour;
+  userForm: FormGroup;
 
   constructor(private tourService: ToursService,
     private route: ActivatedRoute          
@@ -21,6 +33,16 @@ export class OrderComponent implements OnInit {
     this.tourId = this.route.snapshot.paramMap.get('id');
     this.tourService.getTourById(this.tourId).subscribe((tour) => {
       this.tour = tour; 
+    })
+
+    //reactive form
+    this.userForm = new FormGroup({
+      firstName: new FormControl('', {validators: Validators.required}),
+      lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      cardNumber: new FormControl(''),
+      birthDate: new FormControl(''),
+      age: new FormControl(''),
+      citizenship: new FormControl(''),
     })
   }
 }
